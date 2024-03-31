f668c1003e8a471422619b4e55f9587ce89be614
Saifeddine75011 <saifeddine.bejaoui@gmail.com>
2024-03-31 15:23:05 +0200

    Add our custom Listing View Item

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,45 +1,15 @@
-import { StyleSheet, Image } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import Colors from '../../constants/Colors';
-import products from '../../../assets/data/products';
+import { View } from '@/src/components/Themed';
+import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 
 const product = products[1];
 
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>$13.99</Text>
+    <View>
+      <ProductListItem product={products[0]}/>
+      <ProductListItem product={products[1]}/>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  title: 
-  { 
-    fontSize: 18, 
-    fontWeight: '600',
-    marginVertical: 10 
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-});
