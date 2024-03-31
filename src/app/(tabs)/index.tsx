1a3fcc5688f3469f03fbe2eecb89c7789dcedd23
Saifeddine75011 <saifeddine.bejaoui@gmail.com>
2024-03-31 18:55:48 +0200

    Implement FlatList, Link and Pressable for product object

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { FlatList } from 'react-native';
 import { View } from '@components/Themed';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItem';
@@ -7,9 +8,14 @@ const product = products[1];
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]}/>
-      <ProductListItem product={products[1]}/>
+      <FlatList 
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
 
+        />
     </View>
   );
 }
