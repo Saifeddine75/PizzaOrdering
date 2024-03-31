eb60e2ae52f1920ef3c053230685a4046256663b
Saifeddine75011 <saifeddine.bejaoui@gmail.com>
2024-03-31 18:08:09 +0200

    Use safe type for react object and simplify roots by using typescript path alias

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-
 import { useColorScheme } from '../components/useColorScheme';
 
 export {
@@ -22,7 +21,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('@assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
